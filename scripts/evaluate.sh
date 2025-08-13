5aacc7189b87717c3358b132781dafbffa0a9951
Leo Wylonis <lew57@login1.grace.ycrc.yale.edu>
2025-08-12 21:12:01 -0400

    Wrote bash script for evaluate

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=eval_cnn2
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=2G
+#SBATCH --time=02:00:00
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+
+
 python3 src/evaluate.py \
     --dataset rand_sop \
     --model cnn2 \
-    --checkpoint experiments/run_20250722_115422_rand_sop/checkpoints/epoch050.pth
+    --checkpoint experiments/run_20250812_160809_rand_sop/checkpoints/epoch1000.pth \
+    --batch-size 128
