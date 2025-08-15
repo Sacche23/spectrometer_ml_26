c398c986b1f5546c5132d6c8a5e6a1d54b41e186
Leo Wylonis <lwylonis@outlook.com>
2025-08-15 11:31:39 -0400

    Reorganized directory, also created bash script for cross val.

---
+++ scripts/alpha_cross_val.sh
@@ -0,0 +1,12 @@
+python3 -m src.cli.alpha_cross_val \
+    --resp data/responsivity_data/processed/responsivity.npy \
+    --currents data/spectra_data/processed/rand_sop/I_s42.npy \
+    --spectra data/spectra_data/processed/rand_sop/S_s42.npy \
+    --alphas "1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1" \
+    --folds 5 \
+    --downsample 1 \
+    --subset 20 \
+    --metric spectra \
+    --noise-std 1e-3 \
+    --normalize \
+    "$@"
